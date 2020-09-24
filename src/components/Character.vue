f71999c4495da94afbf5f30f9d0f85d6820ebcd6
Basruhe <sb.ruhe@gmail.com>
2020-09-24 17:51:38 +0200

    added more info button

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="col-md-4" @click="switchCharacter">
+  <div class="col-md-4">
     <div class="character-card">
-      <div class="card-block">
+      <div class="card-block center-items" @click="switchCharacter">
         <h4 class="card-title">{{character.name}}</h4>
         <p class="card-text">Height: {{character.height}} cm</p>
         <p class="card-text">Mass: {{character.mass}} kg</p>
         <p class="card-text">Hair Color: {{character.hair_color}}</p>
         <p class="card-text">Eye Color: {{character.hair_color}}</p>
       </div>
+      <button type="button" class="btn btn-dark more-info">More info</button>
     </div>
   </div>
 </template>
